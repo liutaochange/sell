83780c95eae92522fc121f59a95b29803a7d0942
liutaochange <liutaochange@163.com>
2018-05-03 22:35:05 +0800

    header

--- src/base/header/header.vue
+++ src/base/header/header.vue
@@ -13,7 +13,7 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟后送达
         </div>
         <div v-if="seller.supports" class="supports">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
@@ -30,6 +30,9 @@ export default {
       type: Object,
       default: () => {}
     }
+  },
+  created () {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
   }
 }
 </script>
@@ -42,13 +45,18 @@ export default {
     .content
       padding: 24px 12px 18px 24px
       font-size: 0
+      .avatar
+        img
+          border-radius: 4px
       .avatar,.content-item
         display: inline-block
+        vertical-align: top
       .content-item
         font-size: 14px
         margin-left: 16px
         .title
           margin: 2px 0 8px 0
+          font-size: 0
           .brand
             display: inline-block
             vertical-align: top
@@ -62,4 +70,31 @@ export default {
             font-size: 16px
             line-height: 18px
             font-weight: bold
+        .details
+          margin-bottom: 10px
+          line-height: 12px
+          font-size: 12px
+        .supports
+          font-size: 0
+          .icon
+            display: inline-block
+            vertical-align: top
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            font-size: 12px
+            line-height: 12px
 </style>
