482354d79b40911d07c242e57bbe509e824105fc
liutaochange <liutaochange@163.com>
2018-05-06 22:47:45 +0800

    goods component

--- src/api/index.js
+++ src/api/index.js
@@ -9,3 +9,10 @@ export const getSeller = () => {
     return Promise.resolve(res.data)
   })
 }
+// 获取商品信息
+export const getGoods = () => {
+  const url = '/api/goods'
+  return request.get(url).then(res => {
+    return Promise.resolve(res.data)
+  })
+}
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="goods">
-    i am goods
+    <div class="menu-wamp"></div>
+    <div class="goods-wamp"></div>
   </div>
 </template>
 
 <script>
+import {getGoods} from 'api/index'
+const ERROR_OK = 0
 export default {
-  name: 'goods'
+  name: 'goods',
+  props: {
+    seller: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data () {
+    return {
+      goods: []
+    }
+  },
+  created () {
+    this._getGoods()
+  },
+  methods: {
+    _getGoods () {
+      const _this = this
+      getGoods().then((res) => {
+        if (res.errNo === ERROR_OK) {
+          _this.goods = res.data
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wamp
+      flex: 0 0 80px
+      width: 80px
+      background: #f3f5f7
+    .goods-wamp
+      flex: 1
 </style>
