ab7328653de27fcd26d923600867f30d689c7100
liutaochange <liutaochange@163.com>
2018-05-08 23:22:30 +0800

    goods style complete

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -12,23 +12,23 @@
     </div>
     <div class="goods-wamp">
       <ul>
-        <li v-for="(item,index) in goods" :key="index" class="foods-item">
+        <li v-for="(item,index) in goods" :key="index" class="foods-item border-1px">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="(foods,i) in item.foods" class="food-item" :key="i">
               <div class="icon">
-                <img :src="foods.icon" alt="img">
+                <img :src="foods.icon" alt="img" width="57" height="57">
               </div>
               <div class="content">
                 <h2 class="name">{{foods.name}}</h2>
                 <p class="desc">{{foods.description}}</p>
                 <div class="extra">
-                  <span>月售{{foods.sellCount}}份</span>
+                  <span class="count">月售{{foods.sellCount}}份</span>
                   <span>好评率{{foods.rating}}</span>
                 </div>
-                <div class="price">
-                  <span >￥{{foods.price}}</span>
-                  <span v-show="foods.oldPrice">￥{{foods.oldPrice}}</span>
+                <div class="prise">
+                  <span class="now">￥{{foods.price}}</span>
+                  <span class="old" v-show="foods.oldPrie">￥{{foods.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -117,4 +117,51 @@ export default {
           border-1px(rgba(7,17,27,0.1))
     .goods-wamp
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9ddel
+        font-size: 12px
+        color: rgb(147,153,159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7,17,27)
+          .desc,.extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147,153,159)
+          .desc
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .prise
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240,20,20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147,153,159)
 </style>
